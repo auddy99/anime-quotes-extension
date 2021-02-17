e3f35f2dc7dc9ce765fe4d81dfca06a7f3085f2b
auddy <soumyadeepauddy1999@gmail.com>
2021-02-17 14:59:31 +0530

    added 110

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var imgName = parseInt(10*Math.random()+1)
+
+var imgName = parseInt(110*Math.random()+1)
 
 $("#bgImg").attr("src","img/a ("+imgName+").jpg");
 // $("div").css("background","url(img/"+imgName+".jpg)");
